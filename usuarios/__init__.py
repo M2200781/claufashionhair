0d7b0006087da69c26fffdcf3d46317bdc6371ab
M2200781 <163543934+M2200781@users.noreply.github.com>
2024-11-26 14:58:58 -0300

    Update __init__.py

--- usuarios/__init__.py
+++ usuarios/__init__.py
@@ -1,20 +1,14 @@
-#pip install flask-login flask-bcrypt
+pip install flask-login flask-bcrypt
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-import os
 import sqlalchemy
 
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///usuarios.db"
 app.config["SECRET_KEY"] = "07d53d8079db1adde65569a4fc8b467a"
-
-if os.getenv("DATABASE_URL"):
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///usuarios.db"
-
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -22,17 +16,6 @@ login_manager = LoginManager(app)
 
 login_manager.login_view = "index"
 
-from usuarios import models
-
-engine = sqlalchemy.create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
-inspector = sqlalchemy.inspect(engine)
-if not inspector.has_table("usuario"):
-    with app.app_context():
-        database.drop_all()
-        database.create_all()
-        print('Base de dados Criado')
-else:
-    print('Base de dados já existe')
-
 from usuarios import routes
+
 
